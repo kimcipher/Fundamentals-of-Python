bac5d9a292dad2d763243e07b7d5f19ed69ed354
kimcipher <kimutaij192@gmail.com>
2022-10-04 11:27:15 +0300

    first commit

---
+++ lesson1a.py
@@ -0,0 +1,43 @@
+# Commented text
+
+print("Hello World") #output
+
+print(100)
+print("I am 24 years") #foundation
+
+# what to learn
+# variable, datatypes, conditions/control structures, looping, functions, modules
+
+# Object Oriented Programming
+
+# Learn Variables
+# variable is a name used to reference a storage location(memory space storage)
+
+name = "Kim Cipher"
+
+print(name)
+
+age = 24
+print(age)
+school = ""
+print(school) # no error since it's existing
+# variables can be any name given
+# standards used to name variables
+# 1. should start with a lowercase
+# 2. Constants - value never change(should be capitalized)
+
+PI = 3.142
+# 3. No spacing while naming instead replace with underscore or capitalize inner word
+first_name = "Julius"
+firstName = "Julius"
+print(first_name)
+print(firstName)
+
+# Variable Declaration, variable initialization
+school = "Jomo Kenyatta"
+print(school)
+
+# note// Variables are declared on top then used
+
+
+
