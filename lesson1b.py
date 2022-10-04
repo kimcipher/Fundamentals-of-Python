6c82a89f284bdad356af251733be1257bbce9f43
kimcipher <kimutaij192@gmail.com>
2022-10-04 11:45:03 +0300

    data types

---
+++ lesson1b.py
@@ -0,0 +1,21 @@
+# DATA TYPES
+# This is the format of data that a variable is holding
+
+location = "Westlands" # this is a string type thus (str)
+marks = 100 # interger data type thus (int)
+gpa = 150.02 # float data type (float) -numbers with decimals
+
+# Boolean type has either true or false outputs
+
+access = False
+faceLock = True
+
+print(type(location))
+print(type(marks))
+print(type(gpa))
+
+# Reserved words - break, continue
+
+
+
+
