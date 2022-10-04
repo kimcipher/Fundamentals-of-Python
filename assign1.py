7baa81d87808512d68c282c38d506674b2de36d2
kimcipher <kimutaij192@gmail.com>
2022-10-04 11:47:43 +0300

    Basic calculation

---
+++ assign1.py
@@ -0,0 +1,41 @@
+# Algorithms -> steps to follow to achieve a task / step by step procedure of accomplishing a given task
+
+# Simple Interest calculation
+# 1. Store value of principle
+# 2. Store value of rate
+# 3. store value of time
+principle = 20000
+rate = 5
+time = 7
+
+interest = (principle*rate*time) / 100
+
+print("The compound interest is:", interest)
+
+# generating a user input
+principle = int(input("Enter the principle:"))
+rate = int(input("Enter the rate:"))
+time = int(input("Enter the time :"))
+interest = (principle*rate*time) / 100
+
+print("The compound interest is:", interest)
+
+# code to prompt user to enter amount in dollars then convert to shillings
+# exchange rate : 1$ = 176Kshs
+
+# Foreign Exchange Rates
+print("Convert to US dollars")
+Amount = int(input("Enter the amount you wish to convert:"))
+conversion = Amount * 176
+print("the conversion is:", conversion)
+
+# code to calculate the volume of a cylinder
+# PI * r**2 * H
+PI = 3.142
+print("Calculate volume of a cylinder")
+r = int(input("Enter the radius:"))
+H = int(input("Enter the height:"))
+volume = PI * r**2 * H
+print("The volume of the cylinder is:", volume)
+
+
